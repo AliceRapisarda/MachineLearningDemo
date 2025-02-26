3eca511769f9f8cbc623833996da8e9de3bb5221
AliceRapisarda <alice.rapisarda123@gmail.com>
2025-02-26 09:39:22 +0100

    add file classification
    inizio scrittura del codice con i diversi import delle librerie

---
+++ classification.py
@@ -0,0 +1,24 @@
+# --- IMPORT SECTION ---
+import pandas as pd     # to load the data into a DataFrames
+import numpy as np      # for numpy array operations
+import matplotlib.pyplot as plt # to visualization
+#..............importing all the needed functions from scikit-learn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import seaborn as sns
+# --- END OF IMPORT SECTION ---
+
+
+# --- MAIN CODE ---
+#importing del dataframes
+
+
+
+
+
+
+
+# --- END OF MAIN CODE ---
+
